959e0617bb019bf64bebc7d3a75a5e534b88a71f
Brian Douglass <bhdouglass@gmail.com>
2014-03-05 07:44:38 -0500

    Fixed issues with spider crontab

--- spider/run_spider.sh
+++ spider/run_spider.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-python ./spider.py
+/usr/bin/env python ./spider.py
 rm /ubuntu-appstore-cache/spider/* -rf
 cp /ubuntu-appstore/spider /ubuntu-appstore-cache/spider -r
 rm /ubuntu-appstore-cache/spider/* &> /dev/null
