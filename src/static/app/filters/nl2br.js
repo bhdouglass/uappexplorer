8237a564567d0989e5d76f834017fa6315e8acc2
Brian Douglass <bhdouglass@gmail.com>
2014-12-30 00:46:28 -0500

    Made tabs look much better

---
+++ src/static/app/filters/nl2br.js
@@ -0,0 +1,14 @@
+'use strict';
+
+app.filter('nl2br', function($sce){
+  return function(input) {
+    var output = '';
+    if (input) {
+      output = input;
+    }
+
+    output = output.replace('&', '&amp;').replace('>', '&gt;').replace('<', '&lt;');
+    output = output.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br/>$2');
+    return $sce.trustAsHtml(output);
+  }
+});
