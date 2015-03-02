212a6f14e8d00066f7a02fd5b46c9cf903e196e2
Brian Douglass <bhdouglass@gmail.com>
2015-03-02 01:16:32 -0500

    Fixed mistake

--- src/static/app/controllers/mainCtrl.js
+++ src/static/app/controllers/mainCtrl.js
@@ -10,7 +10,7 @@ angular.module('appstore').controller('mainCtrl', function ($scope, $rootScope, 
   function refresh() {
     api.popular().then(function(data) {
       $scope.popular = data;
-    }, function() {
+    }, function(err) {
       console.error(err);
       $rootScope.setError('Could not fetch popular app list, click to retry', refresh);
     });
