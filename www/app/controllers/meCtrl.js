e4dcb6d5e53d548b578a175b864a98c26f821632
Brian Douglass <bhdouglass@gmail.com>
2015-05-27 08:28:24 -0400

    Made list editing mobile friendlier

--- www/app/controllers/meCtrl.js
+++ www/app/controllers/meCtrl.js
@@ -1,69 +1,108 @@
 'use strict';
 
-angular.module('appstore').controller('meCtrl', function($scope, $rootScope, $location, auth, lists, utils) {
+angular.module('appstore').controller('meCtrl', function($scope, $rootScope, $location, $modal, auth, lists, utils) {
   $scope.sorts = utils.sorts;
-  $scope.user = null;
-  $scope.working = true;
-  $scope.lists = [];
-  $scope.defaultNewList = {
-    name: '',
-    sort: '-points',
-    packages: [],
-  };
-  $scope.newList = angular.copy($scope.defaultNewList);
+  $scope.strToColor = utils.strToColor;
 
+  $scope.user = null;
+  $scope.loading = false;
+  $scope.lists = [];
+
+  utils.loading($scope);
   auth.login().then(function(user) {
     $scope.user = user;
 
     return lists.api.findAll();
   })
   .then(function(lists) {
-    $scope.working = false;
+    utils.doneLoading($scope);
     $scope.lists = lists;
   });
 
-  $scope.create = function(list) {
-    $scope.working = true;
+  $scope.icon = function(name) {
+    var icon = 'fa-list-ul';
+    name = name.toLowerCase();
+    if (name.indexOf('favorite') > -1) {
+      icon = 'fa-star';
+    }
+    else if (name.indexOf('love') > -1 || name.indexOf('heart') > -1) {
+      icon = 'fa-heart';
+    }
 
-    lists.api.create(list).then(function() {
-      $scope.newList = angular.copy($scope.defaultNewList);
-      return lists.api.findAll();
-    }, function(err) {
-      console.error(err);
-      $rootScope.setError('Could not create a new list at this time, please try again later');
-    })
-    .then(function(lists) {
-      $scope.working = false;
-      $scope.lists = lists;
+    return icon;
+  };
+
+  $scope.newList = function() {
+    return  {
+      name: '',
+      sort: '-points',
+      packages: [],
+    };
+  }
+
+  var modal = null;
+  $scope.edit = function(list) {
+    $scope.selectedList = angular.copy(list);
+    modal = $modal.open({
+      templateUrl: '/app/partials/listEdit.html',
+      scope: $scope
     });
   };
 
-  $scope.update = function(list) {
-    $scope.working = true;
+  $scope.save = function(list) {
+    if (list.name) {
+      var update = !!list._id
+      utils.loading($scope);
 
-    lists.api.update(list._id, list).then(function() {
-      return lists.api.findAll();
-    }, function(err) {
-      console.error(err);
-      $rootScope.setError('Could not update the list at this time, please try again later');
-    })
-    .then(function(lists) {
-      $scope.working = false;
-      $scope.lists = lists;
+      var promise = null;
+      if (update) {
+        promise = lists.api.update(list._id, list);
+      }
+      else {
+        promise = lists.api.create(list);
+      }
+
+      promise.then(function() {
+        modal.close();
+        return lists.api.findAll();
+      }, function(err) {
+        console.error(err);
+
+        var message = 'Could not create a new list at this time, please try again later';
+        if (update) {
+          message = 'Could not update the list at this time, please try again later';
+        }
+
+        $rootScope.setError(message);
+      })
+      .then(function(lists) {
+        utils.doneLoading($scope);
+        $scope.lists = lists;
+      });
+    }
+  };
+
+  $scope.remove = function(list) {
+    $scope.selectedList = list;
+
+    modal = $modal.open({
+      templateUrl: '/app/partials/listDelete.html',
+      scope: $scope
     });
   };
 
   $scope.delete = function(list) {
-    $scope.working = true;
+    utils.loading($scope);
 
     lists.api.delete(list._id).then(function() {
+      modal.close();
       return lists.api.findAll();
     }, function(err) {
       console.error(err);
       $rootScope.setError('Could not delete the list at this time, please try again later');
     })
     .then(function(lists) {
-      $scope.working = false;
+      utils.doneLoading($scope);
       $scope.lists = lists;
     });
   };
