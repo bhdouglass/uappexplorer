876da55c7f81c99a5f358acf35238006f11e5300
Brian Douglass <bhdouglass@gmail.com>
2015-06-01 00:03:27 -0400

    Cache app list requests

--- www/app/services/api.js
+++ www/app/services/api.js
@@ -1,44 +1,62 @@
 'use strict';
 
 angular.module('appstore').factory('api', function($q, $http) {
+  var apps_cache = {};
+  var apps_cache_limit = [];
+
   return {
     apps: function(paging, canceler) {
       paging = angular.copy(paging);
-      _.forEach(paging.query, function(q) {
-        if (_.isObject(q) && q._$in) {
-          q.$in = q._$in;
-          q._$in = undefined;
-        }
-      });
-
-      var options = {
-        params: paging
-      };
-
-      if (canceler) {
-        options.timeout = canceler.promise;
+      var cache_key = JSON.stringify(paging);
+      if (apps_cache_limit.indexOf(cache_key) > -1) {
+        return $q.when(apps_cache[cache_key]);
       }
+      else {
+        _.forEach(paging.query, function(q) {
+          if (_.isObject(q) && q._$in) {
+            q.$in = q._$in;
+            q._$in = undefined;
+          }
+        });
 
-      var apps_deferred = $q.defer();
-      $http.get('/api/apps', options).then(function(res) {
-        var apps = _.sortBy(res.data.data, paging.sort);
-        apps_deferred.resolve(apps);
-      }, function(err) {
-        apps_deferred.reject(err);
-      });
+        var options = {
+          params: paging
+        };
 
-      var count_deferred = $q.defer();
-      $http.get('/api/apps?count=true', options).then(function(res) {
-        var app_count = res.data.data;
-        count_deferred.resolve(app_count);
-      }, function(err) {
-        count_deferred.reject(err);
-      });
+        if (canceler) {
+          options.timeout = canceler.promise;
+        }
 
-      return $q.all({
-        'apps': apps_deferred.promise,
-        'count': count_deferred.promise,
-      });
+        var apps_deferred = $q.defer();
+        $http.get('/api/apps', options).then(function(res) {
+          var apps = _.sortBy(res.data.data, paging.sort);
+          apps_deferred.resolve(apps);
+        }, function(err) {
+          apps_deferred.reject(err);
+        });
+
+        var count_deferred = $q.defer();
+        $http.get('/api/apps?count=true', options).then(function(res) {
+          var app_count = res.data.data;
+          count_deferred.resolve(app_count);
+        }, function(err) {
+          count_deferred.reject(err);
+        });
+
+        return $q.all({
+          'apps': apps_deferred.promise,
+          'count': count_deferred.promise,
+        }).then(function(data) {
+          apps_cache[cache_key] = data;
+          apps_cache_limit.push(cache_key);
+          if (apps_cache_limit.length > 10) {
+            var remove_key = apps_cache_limit.shift();
+            delete apps_cache_limit[remove_key];
+          }
+
+          return data;
+        });
+      }
     },
 
     count: function(query) {
