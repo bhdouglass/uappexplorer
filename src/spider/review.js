d65eef1124f6d5fad304a328981110bd6f9b674b
Brian Douglass <bhdouglass@gmail.com>
2015-10-02 00:56:33 -0400

    Updated ratings refresh method

--- src/spider/review.js
+++ src/spider/review.js
@@ -10,142 +10,99 @@ var moment = require('moment');
 //Maths from:
 //http://fulmicoton.com/posts/bayesian_rating/
 //http://www.frontendjunkie.com/2011/02/using-bayesian-average-to-rank-content.html
-function calculateBayesianAverages(callback) {
-  db.Package.find({}, function(err, packages) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      var total_num_reviews = 0;
-      var total_rating = 0;
-      var total_packages = 0;
-      _.forEach(packages, function(pkg) {
-        if (pkg.num_reviews > 0) { //Don't count unreviewed apps
-          total_num_reviews += pkg.num_reviews;
-          total_rating += pkg.average_rating;
-          total_packages++;
-        }
-      });
-
-      var total_average_rating = total_rating / total_packages;
-      var average_num_reviews = total_num_reviews / total_packages;
-
-      async.each(packages, function(pkg, cb) {
-        if (pkg.num_reviews > 0) {
-          pkg.bayesian_average = ((average_num_reviews * total_average_rating) + pkg.total_rating) / (average_num_reviews + pkg.num_reviews);
-        }
-        else {
-          pkg.bayesian_average = 0;
-        }
-
-        pkg.save(function(err) {
-          if (err) {
-            cb(err);
-          }
-          else {
-            cb(null);
-          }
-        });
-
-      }, callback);
-    }
-  });
-}
-
-function calculateRatings(pkg, reviews) {
-  var points = 0;
-  var total_rating = 0;
-  var monthly_popularity = 0;
-  _.forEach(reviews, function(review) {
-    total_rating += review.rating;
-
-    if (review.rating == 1) {
-      points -= 1;
-    }
-    else if (review.rating == 2) {
-      points -= 0.5;
-    }
-    else if (review.rating == 4) {
-      points += 0.5;
-    }
-    else if (review.rating == 5) {
-      points += 1;
-    }
-
-    var reviewDate = moment(review.date_created);
-    var now = moment();
-    if (review.rating >= 4 && reviewDate.month() == now.month() && reviewDate.year() == now.year()) {
-      monthly_popularity++;
-    }
-  });
-
-  pkg.points = Math.round(points);
-  pkg.num_reviews = reviews.length;
-  pkg.total_rating = total_rating;
-  pkg.monthly_popularity = monthly_popularity;
-  if (total_rating === 0 || reviews.length === 0) {
-    pkg.average_rating = 0;
-  }
-  else {
-    pkg.average_rating = total_rating / reviews.length;
-  }
-}
-
 function refreshRatings(callback) {
   db.Package.find({}, function(err, packages) {
     if (err) {
       callback(err);
     }
     else {
-      async.eachSeries(packages, function(pkg, callback) {
-        db.Review.findOne({name: pkg.name}, function(err, rev) {
-          if (err) {
-            callback(err);
-          }
-          else if (rev) {
-            calculateRatings(pkg, rev.reviews);
-
-            async.series([
-              function(cb) {
-                rev.save(function(err) {
-                  if (err) {
-                    cb(err);
-                  }
-                  else {
-                    cb(null);
-                  }
-                });
-              },
-              function(cb) {
-                pkg.save(function(err) {
-                  if (err) {
-                    cb(err);
-                  }
-                  else {
-                    cb(null);
-                  }
-                });
-              }
-            ], callback);
-          }
-        });
-      }, function(err) {
+      db.Review.find({}, function(err, reviews) {
         if (err) {
-          logger.error(err);
-          if (callback) {
-            callback(err);
-          }
+          callback(err);
         }
         else {
-          calculateBayesianAverages(function(err) {
-            if (err) {
-              logger.error(err);
+          //Map the reviews by package name for easy access
+          var reviewMap = {};
+          _.forEach(reviews, function(review) {
+            reviewMap[review.name] = review;
+          });
+
+          var total_num_reviews = 0;
+          var total_rating = 0;
+          var total_packages = 0;
+          _.forEach(packages, function(pkg) {
+            logger.debug('reviews - ' + pkg.name);
+            //Calculate heart rating, monthly popularity, and average stars
+            if (reviewMap[pkg.name]) {
+              var review = reviewMap[pkg.name];
+              var points = 0; //Heart rating
+              var total_rating = 0;
+              var monthly_popularity = 0;
+              _.forEach(reviews, function(review) {
+                total_rating += review.rating;
+
+                if (review.rating == 1) {
+                  points -= 1;
+                }
+                else if (review.rating == 2) {
+                  points -= 0.5;
+                }
+                else if (review.rating == 4) {
+                  points += 0.5;
+                }
+                else if (review.rating == 5) {
+                  points += 1;
+                }
+
+                var reviewDate = moment(review.date_created);
+                var now = moment();
+                if (review.rating >= 4 && reviewDate.month() == now.month() && reviewDate.year() == now.year()) {
+                  monthly_popularity++;
+                }
+              });
+
+              pkg.points = Math.round(points);
+              pkg.num_reviews = reviews.length;
+              pkg.total_rating = total_rating;
+
+              pkg.monthly_popularity = monthly_popularity;
+              if (total_rating === 0 || reviews.length === 0) {
+                pkg.average_rating = 0;
+              }
+              else {
+                pkg.average_rating = total_rating / reviews.length;
+              }
             }
 
-            if (callback) {
-              callback(err);
+            //Sum everything for bayesian average
+            if (pkg.num_reviews > 0) { //Don't count unreviewed apps
+              total_num_reviews += pkg.num_reviews;
+              total_rating += pkg.average_rating;
+              total_packages++;
             }
           });
+
+          var total_average_rating = total_rating / total_packages;
+          var average_num_reviews = total_num_reviews / total_packages;
+          async.each(packages, function(pkg, cb) {
+            //Calculate bayesian average for each app
+            if (pkg.num_reviews > 0) {
+              pkg.bayesian_average = ((average_num_reviews * total_average_rating) + pkg.total_rating) / (average_num_reviews + pkg.num_reviews);
+            }
+            else {
+              pkg.bayesian_average = 0;
+            }
+
+            logger.debug('save - ' + pkg.name);
+            pkg.save(function(err) {
+              if (err) {
+                cb(err);
+              }
+              else {
+                cb(null);
+              }
+            });
+          }, callback);
         }
       });
     }
@@ -261,4 +218,3 @@ function parseReviews(pkgName, callback) {
 
 exports.parseReviews = parseReviews;
 exports.refreshRatings = refreshRatings;
-exports.calculateBayesianAverages = calculateBayesianAverages;
